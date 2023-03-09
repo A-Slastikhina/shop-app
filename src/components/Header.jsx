4b9bb751bd7e13efbbdadf3635a2f4d3b74a9143
A-slastikhina <ao.slastikhina@gmail.com>
2023-03-09 11:45:42 +0600

    добавила ссылки на репозиторий

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header (){
   return  <nav className="grey darken-3">
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo right">React Shop App</a>     
-        <div><a href="#!">Repo</a></div>
+      <a href="#!" className="brand-logo right">React Shop</a>     
+        <div><a href="https://github.com/A-Slastikhina/shop-app/">Repo</a></div>
     </div>
   </nav>
 }
